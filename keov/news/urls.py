24bfe09b448f59d376fe8e093cb0c3bbbc1aa6d0
Melkia Mouhamed <melkia02@gmail.com>
2018-05-29 16:34:51 +0200

    Finishing the posting system

--- keov/news/urls.py
+++ keov/news/urls.py
@@ -2,4 +2,6 @@ from django.urls import path, include
 from django.views.generic import ListView, DetailView
 from news.models import Post
 
-urlpatterns = [path('', ListView.as_view(queryset=Post.objects.all().order_by("-date"), template_name="news/news.html"))]
+urlpatterns = [path('', ListView.as_view(queryset=Post.objects.all().order_by("-date"), template_name="news/news.html")),
+               path('news/<int:pk>', DetailView.as_view(model=Post, template_name='news/post.html'))
+               ]
